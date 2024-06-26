e4b253866a0a1c395694e0df05e766a35605f266
moeez-tariq <mt4610@nyu.edu>
2024-06-26 16:26:46 +0400

    okay

--- api/index.js
+++ api/index.js
@@ -13,15 +13,22 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/webhook', (req, res) => {
-    const { sender, date, body } = req.body;
-    if (sender && date && body) {
-        messages.push({ sender, date, body });
-        console.log(`Message received from ${sender} on ${date}: ${body}`);
-        res.status(200).send('Message received');
-    } else {
-        res.status(400).send('Invalid message format');
-    }
+    messages.push(req.body);
+    console.log(`Message received: ${req.body}`);
+    res.status(200).send('Message received');
 });
+
+
+// app.post('/webhook', (req, res) => {
+//     const { sender, date, body } = req.body;
+//     if (sender && date && body) {
+//         messages.push({ sender, date, body });
+//         console.log(`Message received from ${sender} on ${date}: ${body}`);
+//         res.status(200).send('Message received');
+//     } else {
+//         res.status(400).send('Invalid message format');
+//     }
+// });
 
 app.get('/messages', (req, res) => {
     res.json(messages);
