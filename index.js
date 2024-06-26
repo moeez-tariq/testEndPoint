9bd4f31239eb2c217257ecb9e352942175d2dc01
moeez-tariq <mt4610@nyu.edu>
2024-06-26 15:38:28 +0400

    initial basic commit

---
+++ index.js
@@ -0,0 +1,32 @@
+// index.js
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+let messages = [];
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.post('/webhook', (req, res) => {
+    const { sender, date, body } = req.body;
+    if (sender && date && body) {
+        messages.push({ sender, date, body });
+        console.log(`Message received from ${sender} on ${date}: ${body}`);
+        res.status(200).send('Message received');
+    } else {
+        res.status(400).send('Invalid message format');
+    }
+});
+
+app.get('/messages', (req, res) => {
+    res.json(messages);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ public/app.js
@@ -0,0 +1,21 @@
+// public/app.js
+
+async function fetchMessages() {
+    const response = await fetch('/messages');
+    const messages = await response.json();
+    const messagesDiv = document.getElementById('messages');
+    messagesDiv.innerHTML = '';
+    messages.forEach(msg => {
+        const messageDiv = document.createElement('div');
+        messageDiv.className = 'message';
+        messageDiv.innerHTML = `
+            <strong>Sender:</strong> ${msg.sender}<br>
+            <strong>Date:</strong> ${msg.date}<br>
+            <strong>Message:</strong> ${msg.body}
+        `;
+        messagesDiv.appendChild(messageDiv);
+    });
+}
+
+setInterval(fetchMessages, 5000);
+fetchMessages();
