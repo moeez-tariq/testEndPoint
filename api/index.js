005ec2dd8d546974adbcb8f755b714f76fbc760a
moeez-tariq <mt4610@nyu.edu>
2024-06-26 17:42:05 +0400

    added sending mechanism first test

--- api/index.js
+++ api/index.js
@@ -3,11 +3,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const sdk = require('@api/cequens-api');
+require('dotenv').config();
+
 
 const app = express();
 const port = 3000;
 
 let messageList = [];
+
+const authToken = process.env.AUTH_TOKEN;
+sdk.auth(authToken);
+
 
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../public')));
@@ -19,12 +26,32 @@ app.post('/webhook', (req, res) => {
         const contact = contacts[0];
         const message = messages[0];
         const name = contact.profile.name;
-        const wa_id = contact.wa_id;
+        let wa_id = contact.wa_id;
         const timestamp = message.timestamp;
         const body = message.text.body;
-    
+        wa_id = '+' + wa_id;
         messageList.push({ "name":name, "wa_id":wa_id, "timestamp":timestamp, "body":body });
-        console.log(messageList)
+
+        sdk.sendingTemplateMessage({
+            recipient_type: 'individual',
+            type: 'template',
+            template: {
+                language: { policy: 'deterministic', code: 'en' },
+                namespace: '841f4fb9_7e40_4764_b06a_6c323ebba684',
+                components: [{ type: 'body', parameters: [] }],
+                name: 'test_template_101'
+            },
+            to: wa_id,
+        })
+        .then(({ data }) => {
+            console.log(`Template message sent successfully to ${name} (${wa_id})`);
+            console.log(data);
+        })
+        .catch(err => {
+            console.error(`Failed to send template message to ${name} (${wa_id})`);
+            console.error(err);
+        });
+
         res.status(200).send('Message received successfully');
     }
     catch(err){
